55c375d57df6cd7bd274cf42bbc4d136d72eb86e
Harshil Bhatia <harshilbhatia2001@gmail.com>
2020-06-20 23:38:27 +0530

    ico-smart contract

--- migrations/2_deploy_mytoken.js
+++ migrations/2_deploy_mytoken.js
@@ -1,5 +1,8 @@
 const MyToken = artifacts.require("MyToken");
+const Ico = artifacts.require("TokenIco");
 
-module.exports = function(deployer) {
-  deployer.deploy(MyToken,100000000);// multiple arguments can be passed here
+module.exports = function (deployer) {
+  deployer.deploy(MyToken, 100000000).then(() => {
+    return deployer.deploy(Ico, MyToken.address, 10000000000000); // multiple arguments can be passed here
+  }); // multiple arguments can be passed here
 };
